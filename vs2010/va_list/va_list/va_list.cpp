6aa757bb6ace7fb0d3b9ffc037b4785929b22cc0
psi <psi@nasda60@hotmail.com>
2012-03-25 21:27:08 +0900

    add vs2010
    Signed-off-by: psi <psi@nasda60@hotmail.com>

---
+++ vs2010/va_list/va_list/va_list.cpp
@@ -0,0 +1,29 @@
+// va_list.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <string>
+#include <cstdarg>
+#include <cstdio>
+
+
+std::string format(const std::string& format, ...)
+{
+	printf("FORMAT 1 CALLED\n");
+	return ""; /* サンプルなので未実装 */
+}
+std::string format(const std::string& format, va_list ap)
+{
+	printf("FORMAT 2 CALLED\n");
+	return ""; /* サンプルなので未実装 */
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	va_list list;
+	
+	format("format: %s", "Hey!");
+
+	return 0;
+}
+
